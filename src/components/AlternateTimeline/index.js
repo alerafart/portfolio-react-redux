76118130ffeae42db0f2f817952af888ff0596d3
alerafart <maalejandrarafart@gmail.com>
2022-07-15 13:13:29 +0200

    timeline in place wip

--- src/components/AlternateTimeline/index.js
+++ src/components/AlternateTimeline/index.js
@@ -6,91 +6,133 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineDot from '@mui/lab/TimelineDot';
-import FastfoodIcon from '@mui/icons-material/Fastfood';
+import WorkRoundedIcon from '@mui/icons-material/WorkRounded';
 import LaptopMacIcon from '@mui/icons-material/LaptopMac';
-import HotelIcon from '@mui/icons-material/Hotel';
-import RepeatIcon from '@mui/icons-material/Repeat';
+import SchoolRoundedIcon from '@mui/icons-material/SchoolRounded';
 import Typography from '@mui/material/Typography';
+import EmojiEventsRoundedIcon from '@mui/icons-material/EmojiEventsRounded';
 
 
 function AlternateTimeline() {
   return (
     <Timeline position="alternate">
-    <TimelineItem>
-      <TimelineOppositeContent
-        sx={{ m: 'auto 0' }}
-        align="right"
-        variant="body2"
-        color="text.secondary"
-      >
-        9:30 am
-      </TimelineOppositeContent>
-      <TimelineSeparator>
-        <TimelineConnector />
-        <TimelineDot>
-          <FastfoodIcon />
-        </TimelineDot>
-        <TimelineConnector />
-      </TimelineSeparator>
-      <TimelineContent sx={{ py: '12px', px: 2 }}>
-        <Typography variant="h6" component="span">
-          Eat
-        </Typography>
-        <Typography>Because you need strength</Typography>
-      </TimelineContent>
-    </TimelineItem>
-    <TimelineItem>
-      <TimelineOppositeContent
-        sx={{ m: 'auto 0' }}
-        variant="body2"
-        color="text.secondary"
-      >
-        10:00 am
-      </TimelineOppositeContent>
-      <TimelineSeparator>
-        <TimelineConnector />
-        <TimelineDot color="primary">
-          <LaptopMacIcon />
-        </TimelineDot>
-        <TimelineConnector />
-      </TimelineSeparator>
-      <TimelineContent sx={{ py: '12px', px: 2 }}>
-        <Typography variant="h6" component="span">
-          Code
-        </Typography>
-        <Typography>Because it&apos;s awesome!</Typography>
-      </TimelineContent>
-    </TimelineItem>
-    <TimelineItem>
-      <TimelineSeparator>
-        <TimelineConnector />
-        <TimelineDot color="primary" variant="outlined">
-          <HotelIcon />
-        </TimelineDot>
-        <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-      </TimelineSeparator>
-      <TimelineContent sx={{ py: '12px', px: 2 }}>
-        <Typography variant="h6" component="span">
-          Sleep
-        </Typography>
-        <Typography>Because you need rest</Typography>
-      </TimelineContent>
-    </TimelineItem>
-    <TimelineItem>
-      <TimelineSeparator>
-        <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-        <TimelineDot color="secondary">
-          <RepeatIcon />
-        </TimelineDot>
-        <TimelineConnector />
-      </TimelineSeparator>
-      <TimelineContent sx={{ py: '12px', px: 2 }}>
-        <Typography variant="h6" component="span">
-          Repeat
-        </Typography>
-        <Typography>Because this is the life you love!</Typography>
-      </TimelineContent>
-    </TimelineItem>
+      <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: 'auto 0' }}
+            align="right"
+            variant="body2"
+            color="text.secondary"
+            >
+            2022
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            {/* <TimelineConnector sx={{ bgcolor: 'secondary.main' }} /> */}
+            <TimelineDot color="primary">
+              <EmojiEventsRoundedIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: '12px', px: 2 }}>
+            <Typography variant="h6" component="span">
+              Titre Professionnel Développeur Web et Web Mobile
+            </Typography>
+            <Typography>Because this is the life you love!</Typography>
+          </TimelineContent>
+      </TimelineItem>
+
+      <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: 'auto 0' }}
+            variant="body2"
+            color="text.secondary"
+          >
+            2021-2022
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="primary">
+              <LaptopMacIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: '12px', px: 2 }}>
+            <Typography variant="h6" component="span">
+              Formation Développeur Web et Web Mobile
+            </Typography>
+            <Typography>Because it&apos;s awesome!</Typography>
+          </TimelineContent>
+      </TimelineItem>
+
+      <TimelineItem>
+        <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          align="right"
+          variant="body2"
+          color="text.secondary"
+        >
+          2008-2022
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot>
+            <WorkRoundedIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+            Photographe responsable studio E-Commerce
+          </Typography>
+          <Typography>Because you need strength</Typography>
+        </TimelineContent>
+      </TimelineItem>
+
+      <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: 'auto 0' }}
+            variant="body2"
+            color="text.secondary"
+          >
+            2007
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="primary">
+              <EmojiEventsRoundedIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: '12px', px: 2 }}>
+            <Typography variant="h6" component="span">
+              Diplôme d'architecte mention en Urbanisme
+            </Typography>
+            <Typography>Because it&apos;s awesome!</Typography>
+          </TimelineContent>
+      </TimelineItem>
+
+      <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: 'auto 0' }}
+            variant="body2"
+            color="text.secondary"
+          >
+            1999-2006
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="primary">
+              <SchoolRoundedIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: '12px', px: 2 }}>
+            <Typography variant="h6" component="span">
+              Etudes en Architecture
+            </Typography>
+            <Typography>Because it&apos;s awesome!</Typography>
+          </TimelineContent>
+      </TimelineItem>
+      
   </Timeline>
   );
 }
