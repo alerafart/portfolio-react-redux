8a0ff8e880589c8a96e254b6facd22d23edfe69b
alerafart <maalejandrarafart@gmail.com>
2022-08-02 18:42:15 +0200

    detail project description

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -3,7 +3,7 @@ const projects =  [
         {
           "id": 1,
           "name": "Portfolio",
-          "languages": "React, Redux, Sass",
+          "languages": "React, Redux, Sass, MaterialUI",
           link: "https://www.alerafart.com",
           "img": "/static/media/portfolio_alex.133f293c0bcece654318.png"
         },
